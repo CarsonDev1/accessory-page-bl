ebb6b07387a2b49b847ffecd423732adcc1d1971
thanhdi721 <thanhdi721@gmail.com>
2024-09-30 09:18:59 +0700

    update src

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -31,37 +31,32 @@ function CardProduct({
         href={`https://bachlongmobile.com/products/${url_key}`}
         style={{ width: "100%", textDecoration: "none" }}
       >
-
+        <div className="product__price__percent">
+          <p className="product__price--percent-detail">Giảm&nbsp;17%</p>
+        </div>
         <figure className="product__img">
-          <div className="product__price__percent">
-            <p className="product__price--percent-detail">Giảm&nbsp;17%</p>
-          </div>
-          <img
-
-            className="product__img-detail"
-            alt={name}
-            src={image.url}
-          />
+          <img className="product__img-detail" alt={name} src={image.url} />
         </figure>
         <div className="product__title" style={{ textDecoration: "none" }}>
           {name}
         </div>
         <div className="product__groupPrice" style={{ background: "0" }}>
-          <span className="product__price">Giá: {" "}</span>
+          <span className="product__price">Giá: </span>
           <span className="product__priceSpecial">
             {price_range.minimum_price.final_price.value.toLocaleString()}{" "}
             {price_range.minimum_price.final_price.currency}
           </span>
           <span className="product__price_gach">
             {" "}
-            {(price_range.minimum_price.final_price.value + 300000).toLocaleString()}
+            {(
+              price_range.minimum_price.final_price.value + 300000
+            ).toLocaleString()}
           </span>
           <div className="product__con-hang">
             <Image src={iconconhang} alt="" className="product__con-hang-img" />
             <span className="product__con-hang-detail">Còn hàng</span>
           </div>
         </div>
-
       </a>
     </div>
   );
