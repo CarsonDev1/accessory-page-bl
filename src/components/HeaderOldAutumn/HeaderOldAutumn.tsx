aa51dec33cb145c2972791e6a3d74055e606f143
thanhdi721 <thanhdi721@gmail.com>
2024-10-10 14:26:19 +0700

    update

--- src/components/HeaderOldAutumn/HeaderOldAutumn.tsx
+++ src/components/HeaderOldAutumn/HeaderOldAutumn.tsx
@@ -1,14 +1,15 @@
 'use client';
 import React from 'react';
 import Image from 'next/image';
-import bannerPC from '../../../public/HEAD PK THANG 10 1920x500 0410.jpg';
-import bannerMB from '../../../public/HEADER-MOBILE.jpg';
+import bannerPC from '../../../public/THU CU DOI MOI PC.png';
+import bannerMB from '../../../public/THU CU DOI MOI MB.jpg';
 import './HeaderOldAutumn.scss';
 const HeaderOldAutumn = () => {
 	return (
 		<div>
 			<Image src={bannerPC} alt='' className='HeaderOldAutumn-bannerPC' />
 			<Image src={bannerMB} alt='' className='HeaderOldAutumn-bannerMB' />
+
 		</div>
 	);
 };
